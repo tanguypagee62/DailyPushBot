68acff8ac4ec9b308f54fa61700dff8f2e95227e
tanguy.pagee <tanguy.pagee@ett-hvac.com>
2024-05-13 16:06:13 +0200

    Automated daily push

--- script.py
+++ script.py
@@ -24,10 +24,10 @@ def git_push():
     repo.git.push()
 
 # Planifiez l'exécution de la fonction git_push() à minuit chaque jour
-schedule.every().day.at("16:05").do(git_push)
+schedule.every().day.at("16:06").do(git_push)
 schedule.every().day.at("12:00").do(git_push)
 
 # Boucle d'exécution
 while True:
     schedule.run_pending()
-    time.sleep(60)  # Attendez 60 secondes avant de vérifier à nouveau le planning
+    time.sleep(5)  # Attendez 60 secondes avant de vérifier à nouveau le planning
